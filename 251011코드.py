75064f53ebcda173d35dca113f26b71d618929cd
soodal25 <maru70004@gmail.com>
2025-10-18 10:08:02 +0900

    Change font to Malgun Gothic and update logic

--- 251011코드.py
+++ 251011코드.py
@@ -9,8 +9,11 @@ import os
 import matplotlib.font_manager as fm
 import matplotlib.pyplot as plt
 
-plt.rcParams['font.family'] = 'NanumGothic'
+
+plt.rcParams['font.family'] = 'Malgun Gothic'  # Windows의 맑은 고딕
 plt.rcParams['axes.unicode_minus'] = False  # 마이너스 기호 깨짐 방지
+
+	 
 
 if 'running' not in st.session_state:
     st.session_state.running = False
@@ -161,6 +164,7 @@ if goal_sec > 0:
         st.error("목표 시간이 0분입니다.")
     except Exception as e:
         st.error(f"오류가 발생했습니다: {e}")
+
 
 
 
