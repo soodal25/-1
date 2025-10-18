d283aecf391c55a34999465203d07ec34ba8ee7e
soodal25 <maru70004@gmail.com>
2025-10-18 09:45:58 +0900

    Refactor timer logic and update font settings

--- 251011코드.py
+++ 251011코드.py
@@ -6,10 +6,9 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 import os
 
-plt.rcParams['font.family'] ='Malgun Gothic'
-plt.rcParams['axes.unicode_minus'] =False
-
-
+font_location = 'C:\\WINDOWS\\Fonts\\NanumGothicExtraBold.ttf' # For Windows
+font_name = fm.FontProperties(fname=font_location).get_name()
+matplotlib.rc('font', family=font_name)
 
 if 'running' not in st.session_state:
     st.session_state.running = False
@@ -160,5 +159,6 @@ if goal_sec > 0:
         st.error("목표 시간이 0분입니다.")
     except Exception as e:
         st.error(f"오류가 발생했습니다: {e}")
+
 
 
