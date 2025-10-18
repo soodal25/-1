8a70ec49180ad1d7822d95f4805cdaadc5309241
soodal25 <maru70004@gmail.com>
2025-10-18 16:41:54 +0900

    Update 251011코드.py

--- 251011코드.py
+++ 251011코드.py
@@ -163,7 +163,7 @@ if goal_sec > 0:
                 colors=colors_goal,
                 textprops={'fontproperties': fm.FontProperties(fname=font_path) if font_path else None}
             )
-            colors_goal = ["#B2CCFF", "#FAED7D"] 
+                colors_goal = ["#B2CCFF", "#FAED7D"] 
             axs[1].set_title(
     "과목별 공부 비율",
     fontproperties=title_font,
@@ -181,6 +181,7 @@ if goal_sec > 0:
         st.error("목표 시간이 0분입니다.")
     except Exception as e:
         st.error(f"오류가 발생했습니다: {e}")
+
 
 
 
